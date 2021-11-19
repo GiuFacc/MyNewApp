0125fb7a05a726cae2ea326ba8c103823c92f37e
GiuFacc <94678967+GiuFacc@users.noreply.github.com>
2021-11-19 11:34:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,116 +35,118 @@ if uploaded_file is not None:
     dates = df['mm/aaaa'].unique()
 
     with col3:
-    st.header('Visualization settings:')
-    all_names = st.button('Tutti', help='Mostra la tabella pivot intera')
-    clear = st.button('Clear', help='Ripristina le opzioni\n\nVa cliccato dopo tutti')
+        st.header('Visualization settings:')
+        all_names = st.button('Tutti', help='Mostra la tabella pivot intera')
+        clear = st.button('Clear', help='Ripristina le opzioni\n\nVa cliccato dopo tutti')
+        
+        if all_names:
+            name_select = st.multiselect('Nome', sorted(name), sorted(name))
+            place_select = st.multiselect('Luogo', sorted(places), sorted(places))
+            date_Select = st.multiselect('Mese/Anno', sorted(dates), sorted(dates))
 
-    if all_names:
-        name_select = st.multiselect('Nome', sorted(name), sorted(name))
-        place_select = st.multiselect('Luogo', sorted(places), sorted(places))
-        date_Select = st.multiselect('Mese/Anno', sorted(dates), sorted(dates))
-
-        with col1:
-            st.dataframe(df.style.format(precision=1), width=1000, height=704)
-        with col2:
-            table = pd.pivot_table(df, values='Totale', index=['Nome', 'Luogo'],
+            with col1:
+                st.dataframe(df.style.format(precision=1), width=1000, height=704)
+            with col2:
+                table = pd.pivot_table(df, values='Totale', index=['Nome', 'Luogo'],
                                    columns=['mm/aaaa'], aggfunc=np.sum, margins=True,
                                    margins_name='TOTALE')
 
-    else:
-        name_select = st.multiselect('Nome', sorted(name))
-        place_select = st.multiselect('Luogo', sorted(places))
-        date_select = st.multiselect('Mese/Anno', sorted(dates))
+        else:
+            name_select = st.multiselect('Nome', sorted(name))
+            place_select = st.multiselect('Luogo', sorted(places))
+            date_select = st.multiselect('Mese/Anno', sorted(dates))
 
-        if name_select:
-            database = df['Nome'].isin(name_select)
-            table = df['Nome'].isin(name_select)
-            if place_select:
-                database = df['Nome'].isin(name_select) & df['Luogo'].isin(place_select)
-                table = df['Nome'].isin(name_select) & df['Luogo'].isin(place_select)
-                if date_select:
-                    database = df['Nome'].isin(name_select) & df['Luogo'].isin(place_select) & df['mm/aaaa'].isin(
-                        date_select)
-                    table = df['Nome'].isin(name_select) & df['Luogo'].isin(place_select) & df['mm/aaaa'].isin(
-                        date_select)
-            elif date_select:
-                database = df['Nome'].isin(name_select) & df['mm/aaaa'].isin(date_select)
-                table = df['Nome'].isin(name_select) & df['mm/aaaa'].isin(date_select)
+            if name_select:
+                database = df['Nome'].isin(name_select)
+                table = df['Nome'].isin(name_select)
                 if place_select:
-                    database = df['Nome'].isin(name_select) & df['Luogo'].isin(place_select) & df['mm/aaaa'].isin(
+                    database = df['Nome'].isin(name_select) & df['Luogo'].isin(place_select)
+                    table = df['Nome'].isin(name_select) & df['Luogo'].isin(place_select)
+                    if date_select:
+                        database = df['Nome'].isin(name_select) & df['Luogo'].isin(place_select) & df['mm/aaaa'].isin(
                         date_select)
-                    table = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
+                        table = df['Nome'].isin(name_select) & df['Luogo'].isin(place_select) & df['mm/aaaa'].isin(
+                        date_select)
+                elif date_select:
+                    database = df['Nome'].isin(name_select) & df['mm/aaaa'].isin(date_select)
+                    table = df['Nome'].isin(name_select) & df['mm/aaaa'].isin(date_select)
+                    if place_select:
+                        database = df['Nome'].isin(name_select) & df['Luogo'].isin(place_select) & df['mm/aaaa'].isin(
+                        date_select)
+                        table = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
                         date_select)
 
-            df1 = df[table]
-            df2 = df[database]
+                df1 = df[table]
+                df2 = df[database]
 
-            with col1:
-                st.dataframe(df2.style.format(precision=1), width=1000, height=745)
-            with col2:
-                table = pd.pivot_table(df1, values='Totale', index=['Nome', 'Luogo'],
+                with col1:
+                    st.dataframe(df2.style.format(precision=1), width=1000, height=745)
+                with col2:
+                    table = pd.pivot_table(df1, values='Totale', index=['Nome', 'Luogo'],
                                        columns=['mm/aaaa'], aggfunc=np.sum)
 
-        elif place_select:
-            database = df['Luogo'].isin(place_select)
-            table = df['Luogo'].isin(place_select)
-            if name_select:
-                database = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select)
-                table = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select)
-                if date_select:
-                    database = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
-                        date_select)
-                    table = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
-                        date_select)
-            elif date_select:
-                database = df['Luogo'].isin(place_select) & df['mm/aaaa'].isin(date_select)
-                table = df['Luogo'].isin(place_select) & df['mm/aaaa'].isin(date_select)
-                if name_select:
-                    database = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
-                        date_select)
-                    table = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
-                        date_select)
-
-            df1 = df[table]
-            df2 = df[database]
-
-            with col1:
-                st.dataframe(df2.style.format(precision=1), width=1000, height=704)
-
-            with col2:
-                table = pd.pivot_table(df1, values='Totale', index=['Nome', 'Luogo'],
-                                       columns=['mm/aaaa'], aggfunc=np.sum)
-
-        elif date_select:
-            database = df['mm/aaaa'].isin(date_select)
-            table = df['mm/aaaa'].isin(date_select)
-            if name_select:
-                database = df['mm/aaaa'].isin(date_select) & df['Nome'].isin(name_select)
-                table = df['mm/aaaa'].isin(date_select) & df['Nome'].isin(name_select)
-                if place_select:
-                    database = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
-                        date_select)
-                    table = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
-                        date_select)
             elif place_select:
-                database = df['Luogo'].isin(place_select) & df['mm/aaaa'].isin(date_select)
-                table = df['Luogo'].isin(place_select) & df['mm/aaaa'].isin(date_select)
+                database = df['Luogo'].isin(place_select)
+                table = df['Luogo'].isin(place_select)
                 if name_select:
-                    database = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
+                    database = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select)
+                    table = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select)
+                    if date_select:
+                        database = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
                         date_select)
-                    table = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
+                        table = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
+                        date_select)
+                elif date_select:
+                    database = df['Luogo'].isin(place_select) & df['mm/aaaa'].isin(date_select)
+                    table = df['Luogo'].isin(place_select) & df['mm/aaaa'].isin(date_select)
+                    if name_select:
+                        database = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
+                        date_select)
+                        table = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
                         date_select)
 
-            df1 = df[table]
-            df2 = df[database]
+                df1 = df[table]
+                df2 = df[database]
 
-            with col1:
-                st.dataframe(df2.style.format(precision=1), width=1000, height=704)
+                with col1:
+                    st.dataframe(df2.style.format(precision=1), width=1000, height=704)
 
-            with col2:
-                table = pd.pivot_table(df1, values='Totale', index=['Nome', 'Luogo'],
+                with col2:
+                    table = pd.pivot_table(df1, values='Totale', index=['Nome', 'Luogo'],
                                        columns=['mm/aaaa'], aggfunc=np.sum)
 
-    col2.dataframe(table.style.format(formatter="{:.1f}", na_rep='-'), height=704)
+            elif date_select:
+                database = df['mm/aaaa'].isin(date_select)
+                table = df['mm/aaaa'].isin(date_select)
+                if name_select:
+                    database = df['mm/aaaa'].isin(date_select) & df['Nome'].isin(name_select)
+                    table = df['mm/aaaa'].isin(date_select) & df['Nome'].isin(name_select)
+                    if place_select:
+                        database = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
+                        date_select)
+                        table = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
+                        date_select)
+                elif place_select:
+                    database = df['Luogo'].isin(place_select) & df['mm/aaaa'].isin(date_select)
+                    table = df['Luogo'].isin(place_select) & df['mm/aaaa'].isin(date_select)
+                    if name_select:
+                        database = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
+                        date_select)
+                        table = df['Luogo'].isin(place_select) & df['Nome'].isin(name_select) & df['mm/aaaa'].isin(
+                        date_select)
+
+                df1 = df[table]
+                df2 = df[database]
+
+                with col1:
+                    st.dataframe(df2.style.format(precision=1), width=1000, height=704)
+
+                with col2:
+                    table = pd.pivot_table(df1, values='Totale', index=['Nome', 'Luogo'],
+                                       columns=['mm/aaaa'], aggfunc=np.sum)
+
+        col2.dataframe(table.style.format(formatter="{:.1f}", na_rep='-'), height=704)
 
 
+
+    
